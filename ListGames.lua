379a46e312f8ec12e3a22ce487d21a3fa8916268
IamNotDev <hexhubx@gmail.com>
2026-06-18 13:48:27 +0300

    Update ListGames.lua

--- ListGames.lua
+++ ListGames.lua
@@ -2,6 +2,7 @@ local Games = {
   [5523851880] = "https://raw.githubusercontent.com/HexHubX/Main/refs/heads/main/HexHubX%20Ball%20Pool.lua", -- 8-Ball Pool Classic
   [10039338037] = "https://raw.githubusercontent.com/HexHubX/Main/refs/heads/main/Build%20A%20Ring%20Farm.lua", -- Build A Ring Farm
   [107646426076756] = "https://raw.githubusercontent.com/HexHubX/Main/refs/heads/main/Build%20A%20Ring%20Farm.lua", -- Build A Ring Farm (ID2)
+  [10200395747] = "https://raw.githubusercontent.com/HexHubX/Main/refs/heads/main/Grow%20a%20Garden%202.lua", -- Grow a Garden 2
 }
 
 return Games
